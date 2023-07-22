fe465472918efd96238f2f5d8e45cb74c3b47e64
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2023-07-22 09:53:02 -0400

    Update theme switching button

--- scripts.js
+++ scripts.js
@@ -1,14 +1,30 @@
-const init = function() {
- document.documentElement.className = "dark";
+// const init = function() {
+//  document.documentElement.className = "dark";
+// }
+// 
+// const changeTheme = function() {
+//   const rootHTML = document.documentElement;
+//   const newTheme = rootHTML.className === "dark" ? "light" : "dark";
+//   rootHTML.className = newTheme;
+// }
+// 
+// init();
+// 
+// const changeThemeButton = document.getElementById("icon--theme-switch");
+// changeThemeButton.addEventListener("click", changeTheme);
+
+const root = document.documentElement;
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  // dark mode
+  root.className = 'dark';
+}
+else {
+  root.className = 'light';
 }
 
-const changeTheme = function() {
-  const rootHTML = document.documentElement;
-  const newTheme = rootHTML.className === "dark" ? "light" : "dark";
-  rootHTML.className = newTheme;
+const setTheme = function() {
+  const newTheme = root.className === 'dark' ? 'light' : 'dark';
+  root.className = newTheme;
 }
 
-init();
-
-const changeThemeButton = document.getElementById("change-theme");
-changeThemeButton.addEventListener("click", changeTheme);
+document.getElementById('icon--theme-switch').addEventListener('click', setTheme);
